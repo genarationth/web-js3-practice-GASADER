d4bff0d3a902b9efe41900d893f81999e3258b93
GASADER <kajorhkaen@gmail.com>
2023-03-13 16:32:41 +0700

    Complete assessment

---
+++ web-js3-practice.js
@@ -0,0 +1,14 @@
+add = (a, b) => a + b;
+subtract = (a, b) => a - b;
+multiply = (a, b) => a * b;
+divide = (a, b) => a / b;
+
+console.log (add(2,3))
+console.log (subtract(2,3))
+console.log (multiply(2,3))
+console.log (divide(2,3))
+
+myname = (firstName, location, hobby) => {
+    console.log("Hi, my name is " + firstName + " I live in " + location + " and enjoy "+hobby);
+}
+myname('josave', 'hkt', 'eating')
